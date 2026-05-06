44305c4662ac755b7860e3c54b209a24a3dc0eea
Code Crafter <162664649+itxHaroonKhan@users.noreply.github.com>
2026-05-05 19:35:49 -0700

    first commit

--- app.js
+++ app.js
@@ -1,23 +1,71 @@
-// 1. Express ko import kiya
-const express = require('express');
+//without express
+// const http = require('node:http');
 
-// 2. Server initialize kiya
+// const hostname = '127.0.0.1'; //localhost
+// const port = 4000;
+
+// const server = http.createServer((req, res) => {
+//   res.statusCode = 200;
+//   res.setHeader('Content-Type', 'text/plain');
+//   res.end('Welcome to my backend world , aj se backend start!\n');
+// });
+
+
+// server.listen(port, hostname, () => {
+//   console.log(`Server running at http://${hostname}:${port}/`);
+// });
+
+//with express
+
+
+
+
+
+
+// // 1. Express ko import kiya
+// const express = require('express');
+
+// // 2. Server initialize kiya
+// const app = express();
+
+// // 3. Port define kiya
+// const PORT = 3000;
+
+// // 4. Pehla Route (Home Page)
+// app.get('/', (req, res) => {
+//     res.send("<h1>Hello! Backend ki duniya mein khushamdeed.</h1>");
+// });
+
+// // 5. Dusra Route (About Page)
+// app.get('/about', (req, res) => {
+//     res.send("Main ek Express server hoon.");
+// });
+
+// // 6. Server ko start kar diya
+// app.listen(PORT, () => {
+//     console.log(`Server chalu ho gaya hai: http://localhost:${PORT}`);
+// });
+
+const express = require('express');
 const app = express();
 
-// 3. Port define kiya
-const PORT = 3000;
+let port = process.env.PORT || 4000;
 
-// 4. Pehla Route (Home Page)
+
 app.get('/', (req, res) => {
-    res.send("<h1>Hello! Backend ki duniya mein khushamdeed.</h1>");
+  res.send('Welcome to my backend world , aj se backend start!');
 });
 
-// 5. Dusra Route (About Page)
-app.get('/about', (req, res) => {
-    res.send("Main ek Express server hoon.");
-});
+const products = [
+    {id: 1 , name: "Product 1", price: 100},
+    {id: 2 , name: "Product 2", price: 200},
+    {id: 3 , name: "Product 3", price: 300},
+]
 
-// 6. Server ko start kar diya
-app.listen(PORT, () => {
-    console.log(`Server chalu ho gaya hai: http://localhost:${PORT}`);
-});
+app.get('/products', (req , res) =>{
+    res.json({limit: 30 , page: 1 ,products: products});
+})
+
+app.listen(port, () =>{
+    console.log("Server is runnning on port " + port);
+})
