1716769d120cc366d34d83382bec428a76e48a31
Haroon Rasheed <162664649+itxHaroonKhan@users.noreply.github.com>
2026-05-18 12:21:29 -0700

    Update app.js

--- app.js
+++ app.js
@@ -46,26 +46,63 @@
 //     console.log(`Server chalu ho gaya hai: http://localhost:${PORT}`);
 // });
 
-const express = require('express');
-const app = express();
+// const express = require('express');
+// const app = express();
 
-let port = process.env.PORT || 4000;
+// let port = process.env.PORT || 4000;
 
 
-app.get('/', (req, res) => {
-  res.send('Welcome to my backend world , aj se backend start!');
-});
+// app.get('/', (req, res) => {
+//   res.send('Welcome to my backend world , aj se backend start!');
+// });
 
-const products = [
-    {id: 1 , name: "Product 1", price: 100},
-    {id: 2 , name: "Product 2", price: 200},
-    {id: 3 , name: "Product 3", price: 300},
-]
+// const products = [
+//     {id: 1 , name: "Product 1", price: 100},
+//     {id: 2 , name: "Product 2", price: 200},
+//     {id: 3 , name: "Product 3", price: 300},
+// ]
 
-app.get('/products', (req , res) =>{
-    res.json({limit: 30 , page: 1 ,products: products});
+// app.get('/products', (req , res) =>{
+//     res.json({limit: 30 , page: 1 ,products: products});
+// })
+
+// app.listen(port, () =>{
+//     console.log("Server is runnning on port " + port);
+// })
+
+
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.use(express.json())
+
+let notes = [] 
+
+// 1. GET: Saare notes dekhne ke liye
+app.get('/notes', (req, res) => {
+    res.json(notes)
 })
 
-app.listen(port, () =>{
-    console.log("Server is runnning on port " + port);
+// 2. POST: Naya note add karne ke liye
+app.post('/notes', (req, res) => {
+    notes.push(req.body)
+    res.json({ message: "Note added", notes })
 })
+
+// 3. DELETE: Sab se asan tareeqa
+app.delete('/notes/:id', (req, res) => {
+    const id = parseInt(req.params.id)
+    
+    // Us index (id) ko chhor kar baaki saare notes rakh lo
+    notes = notes.filter((note, index) => index !== id)
+    
+    res.json({ message: "Deleted!", notes })
+})
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`)
+})
+
+
+
